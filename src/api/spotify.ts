dab700addb4f5fa2d963d83be7c844f0444653c8
Muhammed Bera Koc <berakoc97@gmail.com>
2021-04-02 23:50:02 +0300

    *Build: Added CORS for server

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -3,6 +3,7 @@ import Playlists from './models/Playlists';
 const API_URL = 'http://zmdp.cloud/iseAlim/spotify.json';
 const GET_REQUEST = {
     method: 'GET',
+    mode: 'cors',
     headers: {
         Accept: 'application/json',
     },
